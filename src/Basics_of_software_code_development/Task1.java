e518eb5c4cdf2eaae29a6985642ea048d970b6e8
Kim Dmitriy <expendebles125@gmail.com>
2022-05-04 09:41:30 +0500

    task1 complete

--- src/Basics_of_software_code_development/Task1.java
+++ src/Basics_of_software_code_development/Task1.java
@@ -1,2 +1,16 @@
-package Basics_of_software_code_development;public class Task1 {
+package Basics_of_software_code_development;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введите значение a");
+        double a = scanner.nextDouble();
+        System.out.println("введите значение b");
+        double b = scanner.nextDouble();
+        System.out.println("введите значение c");
+        double c = scanner.nextDouble();
+        double z = ((a-3)*b/2)+c;
+    }
 }
