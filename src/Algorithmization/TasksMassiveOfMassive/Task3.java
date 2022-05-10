04b3359804b493ea2d8285425c43e827007c66e6
DoAmI <34737681+DoAmI@users.noreply.github.com>
2022-05-10 16:25:19 +0500

    Delete Task3.java

--- src/Algorithmization/TasksMassiveOfMassive/Task3.java
+++
@@ -1,8 +0,0 @@
-package Algorithmization.TasksMassiveOfMassive;
-/* Дана матрица. Вывести k-ю строку и p-й столбец матрицы.*/
-public class Task3 {
-    /*нужны ли комментарии...*/
-    private void printer(int[][] a, int k, int p){
-        System.out.println(a[k][p]);
-    }
-}
